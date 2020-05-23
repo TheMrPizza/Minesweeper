53bdc10b192c82274b89b0349a9346615081bb99
Guy <guycohen08@gmail.com>
2020-05-24 00:22:37 +0300

    Changed game screen colors

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const appConfig = {
     numbersColors: {
         1: "#2278d9",
         2: "#2e9346",
-        3: "#c73b3b",
+        3: "#a71111",
         4: "#3156cb",
         5: "#b12a2a",
         6: "#2b96a3",
